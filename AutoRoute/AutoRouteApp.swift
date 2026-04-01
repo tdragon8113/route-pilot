7bf059e6e4074a3e04c3cce5a88efe103ad62cb0
gary <gary@cashcepat.id>
2026-04-01 17:45:14 +0800

    Initial Commit

---
+++ AutoRoute/AutoRouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoRouteApp.swift
+//  AutoRoute
+//
+//  Created by Sunny Tang on 2026/4/1.
+//
+
+import SwiftUI
+
+@main
+struct AutoRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
